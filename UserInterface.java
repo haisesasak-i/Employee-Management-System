7076b903f3da5e82441d7b42d71d3c540152570d
HAISE SASAKI <154090375+HAISESASAK-I@users.noreply.github.com>
2024-08-20 00:14:15 +0500

    Implement the methods from choice 2 to 5 in UserInterface

--- UserInterface.java
+++ UserInterface.java
@@ -8,9 +8,10 @@ public class UserInterface {
     }
     public void start() {
         int choice;
-        System.out.println(
-            "<------------------------- Employee Manager ----------------------->");
         while (true) {
+            System.out.println(
+                "<------------------------- Employee Manager ----------------------->");
+
             System.out.println("");
             System.out.println(
                 "What would you like to do?\nEnter \"1\" to add a Full-Time or Part-Time employee.\nEnter \"2\" to view details of an employee using Id.\nEnter \"3\" to remove an employee.\nEnter \"4\" to view all employee.\nEnter \"5\" to quit.");
@@ -19,5 +20,29 @@ public class UserInterface {
                 break;
             }
         }
+        if (choice == 1) {
+
+        } else if (choice == 2) {
+            this.displayingDetailsOfEmployeeUsingId();
+        } else if (choice == 3) {
+            this.removeAnEmployeeUsingId();
+        } else if (choice == 4) {
+            displayAllEmployee();
+        } else
+            return;
+    }
+    private void displayAllEmployee() { this.management.displayAllEmployee(); }
+    private void removeAnEmployeeUsingId() {
+        int id = this.gettingIdOfUser();
+        this.management.removeAnEmployee(id);
+    }
+    private void displayingDetailsOfEmployeeUsingId() {
+        int id = this.gettingIdOfUser();
+        this.management.displayDetailsById(id);
+    }
+    private int gettingIdOfUser() {
+        System.out.println("Enter the id of employee to remove:");
+        int id = Integer.valueOf(scanner.nextLine());
+        return id;
     }
 }
