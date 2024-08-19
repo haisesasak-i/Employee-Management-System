b501fe67ed6a514a9ecabf461b85516ed46d0609
HAISE SASAKI <154090375+HAISESASAK-I@users.noreply.github.com>
2024-08-20 00:05:05 +0500

    Create the UserInterface object in Employee Manager

--- EmployeeManager.java
+++ EmployeeManager.java
@@ -1,3 +1,9 @@
+import java.util.Scanner;
 public class EmployeeManager {
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        EmployeeManagement management = new EmployeeManagement();
+        UserInterface userInterface = new UserInterface(scanner, management);
+        userInterface.start();
+    }
 }
